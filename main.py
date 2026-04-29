c7ebc4f52cdcf5e3c54b642aafb0c45ad6618070
dalungao-droid <dalungao@gmail.com>
2026-04-29 16:37:15 +0800

    feat(main): 實作 LangChain LLM 呼叫邏輯

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import os
 from dotenv import load_dotenv
-from langchain.openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
+def main():
+    load_dotenv()
+    aname = "aiaiaaaaaiiiiiiiiiiiiiiiiiiiiiii"
+    api_key = os.getenv("OPENAI_API_KEY")
 
+    if api_key:
+        print("api_key已讀取")
+        
+    else:
+        print("api_key 未被讀取")
+        return
 
+    llm = ChatOpenAI(
+        model=os.getenv("MODEL"),
+        base_url=os.getenv("BASE_URL"),
+        api_key=api_key
+    )
 
+    result = llm.invoke("請自我介紹")
+    print(result.content)
 
-    
 if __name__ == "__main__":
-    aname = "aiaiaaaaaiiiiiiiiiiiiiiiiiiiiiiiii"
-    print(f'my name is {aname}')
+    main()
+    
    
