548e3e90895a4b5b596470e60a1e02e8f3fe0046
dalungao-droid <dalungao@gmail.com>
2026-04-29 16:37:13 +0800

    chore(project): 初始化專案環境與相依套件

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from langchain.openai import ChatOpenAI
+
+
+
+
+
+    
+if __name__ == "__main__":
+    aname = "aiaiaaaaaiiiiiiiiiiiiiiiiiiiiiiiii"
+    print(f'my name is {aname}')
+   
